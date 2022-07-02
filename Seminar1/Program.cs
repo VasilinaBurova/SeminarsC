a24c2accae21a07614a4344b98862612559facb8
Vasilina Burova <vasena28021988@gmail.com>
2022-07-02 12:25:46 +0400

    1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+
+int num, result;
+
+Console.Write("Input integer number:");
+num = Convert.ToInt32(Console.ReadLine());
+
+result = num * num;
+
+Console.WriteLine("Result is " + result);
+
+*/
+
+/*
+
+int num1, num2, quad;
+
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+quad = num2 * num2;
+
+if(num1 == quad)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+*/
