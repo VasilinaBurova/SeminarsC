c86be74b4f8f4ff79947ec098c4beb7b7aefe9c7
Vasilina Burova <vasena28021988@gmail.com>
2022-07-02 12:25:21 +0400

    1

---
+++ HomeWorkSem1/Program.cs
@@ -0,0 +1,100 @@
+﻿//Task number one 
+
+/*
+int num1, num2;
+
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>num2)
+{
+    Console.WriteLine(max = num1);
+}
+else
+{
+    Console.WriteLine(max = num2);
+}
+*/
+
+//Task number two
+/*
+int num1,num2,num3;
+
+Console.Write("input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input three integer number: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num2 > num3)
+{
+    if(max > num2)
+    {
+        Console.WriteLine(max);
+    }
+    else
+    {
+        Console.WriteLine(max = num2);
+    }
+}
+else
+{
+    if(max > num3)
+    {
+        Console.WriteLine(max);
+    }
+    else
+    {
+        Console.WriteLine(max = num3);
+    }
+}
+*/
+
+//Task number three
+
+/*
+int num;
+
+Console.Write("input number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+*/
+
+//Task four
+
+/*
+int num;
+
+Console.Write("Input number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while (current <= num)
+{
+    if(current % 2 == 0)
+    {
+        Console.Write(current + " ");
+    }
+    else
+    {
+    }
+    current = current + 1;
+}
+*/
